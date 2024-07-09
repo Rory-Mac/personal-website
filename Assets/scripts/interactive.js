281b8a681b34b6fad3619fbf1c8dd8f045a938f5
Rory Maclean <roryobm@outlook.com>
2024-07-09 20:05:06 +1000

    add swipe functionality for navigation

--- Assets/scripts/interactive.js
+++ Assets/scripts/interactive.js
@@ -1,28 +1,27 @@
 var _startX = 0;
 var _startY = 0;
-var _offsetX = 0;			
+var _offsetX = 0;
 var _offsetY = 0;
-var _dragElement;
+var _dragElement = document.getElementById('portal_frame');
 var _isDragging = false;
 var _dragThreshold = 5;
 
 document.onmousedown = OnMouseDown;
 document.onmouseup = OnMouseUp;
 
-function OnMouseDown(event){
+function OnMouseDown(event) {
     event.preventDefault();
 
     _startX = event.clientX;
     _startY = event.clientY;
     _offsetX = document.getElementById('portal_frame').offsetLeft;
     _offsetY = document.getElementById('portal_frame').offsetTop;
-    _dragElement = document.getElementById('portal_frame');
     _isDragging = false;
 
     document.onmousemove = OnMouseMove;
 }
 
-function OnMouseMove(event){
+function OnMouseMove(event) {
     var moveX = event.clientX - _startX;
     var moveY = event.clientY - _startY;
     if (Math.abs(moveX) > _dragThreshold || Math.abs(moveY) > _dragThreshold) {
@@ -32,13 +31,22 @@ function OnMouseMove(event){
     }
 }
 
-function OnMouseUp(){
-	document.onmousemove = null;
+function OnMouseUp() {
+    document.onmousemove = null;
 }
 
 document.addEventListener('click', function(event) {
     if (_isDragging) {
         event.preventDefault();
     }
-	_isDragging = false;
-}, true);
+    _isDragging = false;
+});
+
+document.addEventListener('wheel', function(event) {
+    console.log(event.deltaY)
+    event.preventDefault();
+    _offsetX = document.getElementById('portal_frame').offsetLeft;
+    _offsetY = document.getElementById('portal_frame').offsetTop;
+    _dragElement.style.left = (_offsetX - event.deltaX) + 'px';
+    _dragElement.style.top = (_offsetY - event.deltaY) + 'px';
+}, {passive: false});
