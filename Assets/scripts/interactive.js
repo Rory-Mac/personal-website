42475d587768fc9eec43f4c024ba36d9a2166ac4
Rory Maclean <roryobm@outlook.com>
2024-07-09 13:18:37 +1000

    improve click-and-drag functionality

--- Assets/scripts/interactive.js
+++ Assets/scripts/interactive.js
@@ -3,25 +3,42 @@ var _startY = 0;
 var _offsetX = 0;			
 var _offsetY = 0;
 var _dragElement;
+var _isDragging = false;
+var _dragThreshold = 5;
 
 document.onmousedown = OnMouseDown;
 document.onmouseup = OnMouseUp;
 
 function OnMouseDown(event){
-	document.onmousemove = OnMouseMove;
+    event.preventDefault();
+
     _startX = event.clientX;
-	_startY = event.clientY;
-	_offsetX = document.getElementById('portal_frame').offsetLeft;
-	_offsetY = document.getElementById('portal_frame').offsetTop;
-	_dragElement = document.getElementById('portal_frame');
+    _startY = event.clientY;
+    _offsetX = document.getElementById('portal_frame').offsetLeft;
+    _offsetY = document.getElementById('portal_frame').offsetTop;
+    _dragElement = document.getElementById('portal_frame');
+    _isDragging = false;
+
+    document.onmousemove = OnMouseMove;
 }
 
 function OnMouseMove(event){
-	_dragElement.style.left = (_offsetX + event.clientX - _startX) + 'px';
-	_dragElement.style.top = (_offsetY + event.clientY - _startY) + 'px';
+    var moveX = event.clientX - _startX;
+    var moveY = event.clientY - _startY;
+    if (Math.abs(moveX) > _dragThreshold || Math.abs(moveY) > _dragThreshold) {
+        _isDragging = true;
+        _dragElement.style.left = (_offsetX + moveX) + 'px';
+        _dragElement.style.top = (_offsetY + moveY) + 'px';
+    }
 }
 
-function OnMouseUp(event){
+function OnMouseUp(){
 	document.onmousemove = null;
-	_dragElement=null;
 }
+
+document.addEventListener('click', function(event) {
+    if (_isDragging) {
+        event.preventDefault();
+    }
+	_isDragging = false;
+}, true);
