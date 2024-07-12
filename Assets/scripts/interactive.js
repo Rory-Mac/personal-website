f278165cc3242a7e25a67f08739eeee587f9c170
Rory Maclean <roryobm@outlook.com>
2024-07-12 16:10:35 +1000

    add zoom and pinch functionality to home page

--- Assets/scripts/interactive.js
+++ Assets/scripts/interactive.js
@@ -1,13 +1,15 @@
 var _startX = 0;
 var _startY = 0;
-var _offsetX = 0;
-var _offsetY = 0;
-var _dragElement = document.getElementById('portal_frame');
 var _isDragging = false;
 var _dragThreshold = 5;
 
 document.onmousedown = OnMouseDown;
 document.onmouseup = OnMouseUp;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var _dragElement = document.getElementById('portal_frame');
+    _dragElement.style.zoom = '100%';
+});
 
 function OnMouseDown(event) {
     event.preventDefault();
@@ -40,13 +42,18 @@ document.addEventListener('click', function(event) {
         event.preventDefault();
     }
     _isDragging = false;
-});
+}, {passive: false});
 
 document.addEventListener('wheel', function(event) {
-    console.log(event.deltaY)
     event.preventDefault();
-    _offsetX = document.getElementById('portal_frame').offsetLeft;
-    _offsetY = document.getElementById('portal_frame').offsetTop;
-    _dragElement.style.left = (_offsetX - event.deltaX) + 'px';
-    _dragElement.style.top = (_offsetY - event.deltaY) + 'px';
+    // indicates pinch/zoom motion
+    if (event.ctrlKey) {
+        scale = 1 - (event.deltaY / 100);
+        _dragElement.style.zoom = (parseFloat(_dragElement.style.zoom) * scale) + '%';
+        _dragElement.style.left = (parseFloat(_dragElement.style.left) - (scale * event.clientX - event.clientX)) + 'px';
+        _dragElement.style.top = (parseFloat(_dragElement.style.top) - (scale * event.clientY - event.clientY)) + 'px';
+    }
+    // else indicates scrolling motion
+    _dragElement.style.left = (parseFloat(_dragElement.style.left) - event.deltaX) + 'px';
+    _dragElement.style.top = (parseFloat(_dragElement.style.top) - event.deltaY) + 'px';
 }, {passive: false});
